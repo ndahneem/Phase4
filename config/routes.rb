fa58c077d5e66ce485987a558817e82f95857811
Noor <ndahneem@andrew.cmu.edu>
2018-04-05 10:18:14 +0000

    add files

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  resources :locations
+  resources :instructors
+  resources :curriculums
+  resources :camp_instructors
+  resources :camps
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
---
+++ test/models/camp_instructor_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class CampInstructorTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
